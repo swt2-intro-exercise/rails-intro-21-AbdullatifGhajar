64685ee4bd2b2f5cd1669409af8683b1dcb4eb08
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2021-11-24 23:23:55 +0100

    Save author after submitting form

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,15 @@
 class AuthorsController < ApplicationController
     def new
+        @author = Author.new
+    end
+
+    def create
+        Author.create(author_params)
+        render :new
+    end
+
+    private
+    def author_params
+        params.require(:author).permit(:first_name, :last_name, :homepage)
     end
 end
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -17,4 +17,26 @@ describe "New author page", type: :feature do
     visit new_author_path
     expect(page).to have_css('input[type="submit"]')
   end
+
+  it "should work when submitting a filled-out form" do
+    visit new_author_path
+    fill_in "author[first_name]", with: "Alan"
+    fill_in "author[last_name]", with: "Turing"
+    fill_in "author[homepage]", with: 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+  end
+
+  it "should create a new author in the database after submitting the form" do
+    author = Author.where(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing").take
+    expect(author).to be_nil
+
+    visit new_author_path
+    fill_in "author[first_name]", with: "Alan"
+    fill_in "author[last_name]", with: "Turing"
+    fill_in "author[homepage]", with: 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+
+    author = Author.where(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing").take
+    expect(author).not_to be_nil
+  end
 end
