b832c0c455eb2013344a2d4787da74852cbffa49
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2021-11-25 14:45:51 +0100

    Show error of invalid last name

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,23 +1,28 @@
 class AuthorsController < ApplicationController
-    def index
-      @authors = Author.all
-    end
-    
-    def show
-      @author = Author.find(params[:id])
-    end
-    
-    def new
-        @author = Author.new
-    end
+  def index
+    @authors = Author.all
+  end
 
-    def create
-        Author.create(author_params)
-        render :new
-    end
+  def show
+    @author = Author.find(params[:id])
+  end
 
-    private
-    def author_params
-        params.require(:author).permit(:first_name, :last_name, :homepage)
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+
+    if @author.save
+      redirect_to @author
+    else
+      render :new
     end
+  end
+
+  private
+  def author_params
+      params.require(:author).permit(:first_name, :last_name, :homepage)
+  end
 end
--- spec/features/author/new_spec.rb
+++ spec/features/author/new_spec.rb
@@ -39,4 +39,25 @@ describe "New author page", type: :feature do
     author = Author.where(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing").take
     expect(author).not_to be_nil
   end
+
+  it "should show the error after submitting with no last name" do
+    visit new_author_path
+    expect(page).to_not have_text("Last name can't be blank")
+
+    fill_in "author[first_name]", with: "Alan"
+    fill_in "author[homepage]", with: 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+
+    expect(page).to have_text("invalid input")
+  end
+
+  it "should not save the new author if last name is missing" do
+    visit new_author_path
+    fill_in "author[first_name]", with: "Alan"
+    fill_in "author[homepage]", with: 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+
+    author = Author.where(first_name: "Alan", homepage: "http://wikipedia.org/Alan_Turing").take
+    expect(author).to be_nil
+  end
 end
