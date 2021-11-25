c704f7edd46edad8e00572d28db434283930d94a
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2021-11-25 14:13:21 +0100

    Add last name validation for authors

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
     it "should have a constructor to create new author object" do
-        expect(Author.new).to be_valid
-
         author = Author.new
         expect(author).to be_an_instance_of(Author)
+    end
+
+    
+    it "should not be valid without last name" do
+        author = build :author
+        expect(author).to be_valid
+
+        author.last_name = ""
+        expect(author).to_not be_valid
+
+        author.last_name = nil
+        expect(author).to_not be_valid
     end
 
     it "should should be able to set attributes before after creating" do
